db350faed7ba26c3f13831050fef8b900f2cdce6
Rafaela <oliveirarafaela865@gmail.com>
2022-10-11 10:33:03 -0300

    finalizando o projeto

--- calculadora/script.js
+++ calculadora/script.js
@@ -2,3 +2,21 @@ function insert (num) {
     var numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero + num;
 }
+
+function limpar () {
+    document.getElementById('resultado').innerHTML = "";
+}
+
+function apagaUmPorVez () {
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1)
+}
+function calcular() {
+    var resultado = document.getElementById('resultado').innerHTML;
+    if(resultado) {
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+    else {
+        document.getElementById('resultado').innerHTML = "Nada para calcular"
+    }
+}
